65c3be13f93775631b33f18f62bd827cb01a2cc5
Wera Barinova <alisawera@yandex.ru>
2025-07-16 16:23:02 +0300

    Урок 4. Директива ветвления

--- Lesson4/money/server.py
+++ Lesson4/money/server.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()    # создаём инстанцию FastAPI
 templates = Jinja2Templates(directory='templates')
 
 @app.get("/")      # определяем запрос GET для пути "/"
-async def root(request):  # определяем функцию (с произвольным именем!), которая обрабатывает этот запрос
+async def root(request: Request):  # Домашняя страница
     return templates.TemplateResponse(
         name='my_template.html',
         context={
             'request': request,
             'title': 'Самый лучший в мире сайт'})
+            
+            
+@app.get("/profile")      # определяем запрос GET для пути "/profile"
+async def profile(request: Request):  # пользователь
+    return templates.TemplateResponse(
+        name='user.html',
+        context={
+            'request': request,
+            'user': {'is_admin': True}})
